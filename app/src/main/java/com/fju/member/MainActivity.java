a919b7cc022e1ae6abcda783223c2a2387cbdb7f
MIIA <wendy89711>
2020-01-07 14:02:49 +0800

    1st_無SharedPreferences，自動開啟NicknameActivity。

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,14 +2,19 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_CODE_LOGIN = 1;
+    boolean logon = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String text;
+        if (!logon) {
+            Intent login = new Intent(this,NicknameActivity.class);
+            startActivityForResult(login,REQUEST_CODE_LOGIN);
+        }
     }
 }
